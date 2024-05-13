3752422d283a9d3c8bcc3edfd15fa7a102994e88
Anna Lyakhova <mintteaann@gmail.com>
2024-05-13 17:30:24 +0400

    upd test

--- test.php
+++ test.php
@@ -1,53 +1,22 @@
 <?php
-use PHPUnit\Framework\TestCase;
-use Facebook\WebDriver\WebDriverBy;
-use Facebook\WebDriver\WebDriverExpectedCondition;
 
-class ThanksTest extends TestCase
-{
-    protected $webDriver;
+require 'thanks.js';
 
-    public function setUp(): void
-    {
-        $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', [
-            'platform' => 'LINUX',
-            'browserName' => 'chrome',
-        ]);
-    }
+// Функция для проверки добавления класса "open"
+function testAddClass() {
+    $_GET['open-modal'] = 'click'; // Симулируем событие click
+    ob_start(); 
+    include 'thanks.js'; 
+    $output = ob_get_clean();
 
-    public function tearDown(): void
-    {
-        $this->webDriver->quit();
-    }
-
-    public function testOpenModalOnClick()
-    {
-        $this->webDriver->get('http://minttey3.beget.tech/');
-        $openModalButton = $this->webDriver->findElement(WebDriverBy::id('open-modal'));
-        $openModalButton->click();
-
-        $modalBoxThanks = $this->webDriver->findElement(WebDriverBy::id('modal-box-thanks'));
-        $this->assertTrue($modalBoxThanks->getAttribute('class') === 'open');
-    }
-
-    public function testCloseModalOnClick()
-    {
-        $this->webDriver->get('http://minttey3.beget.tech/');
-        $closeModalButton = $this->webDriver->findElement(WebDriverBy::id('close_modal'));
-        $closeModalButton->click();
-
-        $modalBoxThanks = $this->webDriver->findElement(WebDriverBy::id('modal-box-thanks'));
-        $this->assertTrue($modalBoxThanks->getAttribute('class') !== 'open');
-    }
-
-    public function testCloseModalOutsideClick()
-    {
-        $this->webDriver->get('http://minttey3.beget.tech/');
-        // Эмулируем клик вне модального окна
-        $body = $this->webDriver->findElement(WebDriverBy::tagName('body'));
-        $body->click();
-
-        $modalBoxThanks = $this->webDriver->findElement(WebDriverBy::id('modal-box-thanks'));
-        $this->assertTrue($modalBoxThanks->getAttribute('class') !== 'open');
+    // Проверяем наличие класса "open" в соответствующем элементе
+    if (document.getElementById("modal-box-thanks").classList.remove("open")) {
+        echo "Тест успешно пройден: класс 'open' был добавлен.";
+    } else {
+        echo "Тест не пройден: класс 'open' не был добавлен.";
     }
 }
+
+testAddClass();
+
+?>
